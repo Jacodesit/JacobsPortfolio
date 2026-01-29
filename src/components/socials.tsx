a2f4da0308fdd2a324a552f921aee86d223de1c7
AkoSiJacobDeveloper <pauljacobtocmo96@gmail.com>
2026-01-29 11:03:57 +0800

    Initial fix this time

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -4,37 +4,27 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Socials() {
     const socials = [
-        { name: 'Facebook', url: 'https://www.facebook.com/pauljacob.tocmo/', icon: <FaFacebook size={24} /> },
-        { name: 'Instagram', url: 'https://www.instagram.com/paaul_jcb/', icon: <FaInstagram size={24} /> },
-        { name: 'GitHub', url: 'https://github.com/Jacodesit', icon: <FaGithub size={24} /> },
+        { name: 'Facebook', url: 'https://www.facebook.com/pauljacob.tocmo/', icon: <FaFacebook size={20} /> },
+        { name: 'Instagram', url: 'https://www.instagram.com/paaul_jcb/', icon: <FaInstagram size={20} /> },
+        { name: 'GitHub', url: 'https://github.com/Jacodesit', icon: <FaGithub size={20} /> },
     ];
 
     return (
         <aside className="">
-            <ul className="flex flex-col gap-3">
+            <ul className="flex flex-col gap-2">
                 {socials.map(social => (
-                    <li key={social.name}>
+                    <li 
+                        key={social.name} 
+                    >
                         <a 
                             href={social.url} 
                             target="_blank" 
-                            rel="noopener noreferrer"
-                            className="
-                                flex items-center gap-3 
-                                bg-[#1a1a1a] p-4 rounded 
-                                transition-all duration-200 
-                                hover:scale-105 active:scale-95 active:bg-[#2a2a2a]
-                                cursor-pointer touch-manipulation
-                                select-none
-                                min-h-[12]
-                            "
-                            style={{
-                                WebkitTapHighlightColor: 'transparent', // Removes iOS tap highlight
-                            }}
+                            rel="noopener noreferrer" 
+                            className="flex items-center gap-2 bg-[#1a1a1a] p-3 lg:p-4 rounded transition-all duration-500 hover:scale-105 hover:cursor-pointer z-99"
+
                         >
                             {social.icon}
-                            <span className="text-sm font-extralight">
-                                {social.name}
-                            </span>
+                            <span className="text-sm font-extralight">{social.name}</span>
                         </a>
                     </li>
                 ))}
