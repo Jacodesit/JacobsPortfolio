3b315fde40473e3360995a45e9b1c4deda2a157b
AkoSiJacobDeveloper <pauljacobtocmo96@gmail.com>
2026-02-25 10:55:15 +0800

    Initial commit

--- src/components/back-btn.tsx
+++ src/components/back-btn.tsx
@@ -5,8 +5,11 @@ export default function BackBtn() {
     const navigate = useNavigate();
     
     return (
-        <div onClick={() => navigate(-1)}>
-            <FaArrowCircleLeft className="hover:cursor-pointer" />
+        <div className="z-9999" >
+            <FaArrowCircleLeft
+                onClick={() => navigate('/home')}
+                className="cursor-pointer" 
+            />
         </div>
     )
 }
