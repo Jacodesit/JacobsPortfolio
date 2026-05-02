dece227a50376b738e45d104702aeb1486672e28
AkoSiJacobDeveloper <pauljacobtocmo96@gmail.com>
2026-05-02 20:46:22 +0800

    Make the action button in left component functional

--- src/components/action-btn.tsx
+++ src/components/action-btn.tsx
@@ -4,19 +4,21 @@ import DownloadIcon from "../components/download-icon";
 export default function ActionBtn() {
     return (
         <div className="flex justify-center gap-1 mx-auto">
-            <button
+            <a
+                href="/Tocmo_Resume.pdf"
                 className="flex items-center gap-1 bg-[#FFFFFF0F] p-2 px-3 rounded text-xs"
             >   
                 <DownloadIcon />
                 Download CV
-            </button>
+            </a>
 
-            <button
+            <a
+                href="mailto:pauljacobtocmo96@gmail.com?subject=Job Opportunity&body=Hello, I saw your portfolio." target="_blank"
                 className="flex items-center gap-1 bg-[#FFFFFF0F] p-2 px-3 rounded text-xs"
             >   
                 <EmailIcon />
                 Email Me
-            </button>
+            </a>
         </div>
     )
 }
